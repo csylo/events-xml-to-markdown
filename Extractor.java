1bf20b1346e1c04151da0f64c33d22eb0dc620e5
clo355 <lo.christopher.355@gmail.com>
2018-05-19 15:37:09 -0400

    90%

--- Extractor.java
+++ Extractor.java
@@ -1,7 +1,10 @@
-import java.util.Scanner;
 import java.util.ArrayList;
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
 
 /*
  * Get all event html content and put them into markdown files
@@ -15,23 +18,70 @@ public class Extractor{
 	    Scanner scan = new Scanner(inputFile);
 	    ArrayList<String> myList = new ArrayList<String>();
 
-	    //reaching a tag sets it up to save whatever line is under it
-	    //as title, author, date, content, etc.
 	    boolean expectTitle = false;
+	    boolean expectPubDate = false;
+	    boolean expectContents = false;
 
 	    while(scan.hasNextLine()){
 		myList.add(scan.nextLine());
 	    }
 	    for(String line : myList){
-		if(expectTitle){
-		    //there are 2 tabs before title-tag
-		    String eventTitle = line.substring(9, line.length() - 8);
-		    String fileName = toFileName(eventTitle);
-		    System.out.println("Event title: " + eventTitle + "      File name: " + fileName);
+		String eventTitle = "";
+		String fileName = "";
+		String pubDate = "";
+		String contents = "";
+		if(expectTitle && line.contains("<title>")){
+		    //there are 2 tabs before html tags
+		    eventTitle = line.substring(9, line.length() - 8);
+		    fileName = toFileName(eventTitle);
 		    expectTitle = false;
-		}
-		if(line.contains("<item>")){
+		    expectPubDate = true;
+		} else if(expectPubDate && line.contains("<pubDate>")){
+		    pubDate = line.substring(11, line.length() - 10);
+		    expectPubDate = false;
+		    expectContents = true;
+		} else if(line.contains("<content:encoded>")){
+		    expectContents = true;
+		} else if(expectContents && (!line.contains("<![CDATA["))){
+		    if(line.contains("]]>")){
+			//this line is CDATA close
+			expectContents = false;
+		    } else{
+			//this line is content
+			contents = line;
+		    }
+		} else if(line.contains("<item>")){
 		    expectTitle = true;
+		}
+
+		//output to md file
+		try{
+		    String filePath = "md-events/" + fileName;
+		    File mayNotExist = new File(filePath);
+		    if (!mayNotExist.exists()) {
+			mayNotExist.createNewFile();
+		    }
+		    FileWriter fw = new FileWriter(filePath);
+		    BufferedWriter bw = new BufferedWriter(fw);
+		    try{
+			bw.write("---\n");
+			bw.write("title: " + eventTitle + "\n");
+			bw.write("date: " + pubDate + "\n");
+			bw.write("layout: post\n\n");
+			bw.write("---\n");
+			bw.write(contents);
+		    } catch(IOException e){
+			System.out.println("@@@IOException 1");
+		    } finally{
+			try{
+			    fw.close();
+			    bw.close();
+			} catch(IOException e){
+			    System.out.println("@@@IOException 2");
+			}
+		    }
+		} catch(IOException e){
+		    System.out.println("@@@IOException 3");
 		}
 	    }
 	    scan.close();
@@ -42,15 +92,27 @@ public class Extractor{
     }
 
     public static String toFileName(String title){
+	//format event title to file name conventions
 	title = title.toLowerCase();
 	title = title.replace(' ', '-');
 	title = title.replace("@", "at");
 	title = title.replace(":", "");
-	//if(file(title + ".md") already exists in md-events){
-	//    title = title.concat("-2.md");
-	//} else{
-	        title = title.concat(".md");
-	//}
+	title = title.replace("&#039;", "");
+
+	//don't overwrite existing md event if it has same name
+	File mayExist = new File("md-events/" + title + ".md");
+	
+	if(mayExist.exists()){
+	    title = title.concat("-2.md");
+	} else{
+	    title = title.concat(".md");
+	}
+	
 	return title;
+    }
+
+    public static String toFMdate(String date){
+	//format wp date to front matter date format
+	return date;
     }
 }

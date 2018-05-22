ee1ff38b684e90bab76bd9b8770627e920687ed2
clo355 <lo.christopher.355@gmail.com>
2018-05-22 00:29:19 -0400

    output as posts to md-events directory

--- Extractor.java
+++ Extractor.java
@@ -1,13 +1,14 @@
 import java.util.ArrayList;
-import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.io.FileWriter;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.Scanner;
 
 /*
  * Get all event html content and put them into markdown files
+ *
+ * Make sure wp exported xml's post years are not -0001
  *
  */
 
@@ -22,85 +23,108 @@ public class Extractor{
 	    boolean expectPubDate = false;
 	    boolean expectContents = false;
 
+	    String eventTitle = "";
+	    String fileName = "";
+	    String pubDate = "";
+	    String contents = "";
+
+	    //output to md-events directory
+	    new File("md-events").mkdirs();
+
 	    while(scan.hasNextLine()){
 		myList.add(scan.nextLine());
 	    }
 	    for(String line : myList){
-		String eventTitle = "";
-		String fileName = "";
-		String pubDate = "";
-		String contents = "";
 		if(expectTitle && line.contains("<title>")){
 		    //there are 2 tabs before html tags
 		    eventTitle = line.substring(9, line.length() - 8);
-		    fileName = toFileName(eventTitle);
 		    expectTitle = false;
 		    expectPubDate = true;
 		} else if(expectPubDate && line.contains("<pubDate>")){
 		    pubDate = line.substring(11, line.length() - 10);
+		    fileName = toFileName(eventTitle, pubDate);
+		    pubDate = toFrontMatterDate(pubDate);
 		    expectPubDate = false;
-		    expectContents = true;
 		} else if(line.contains("<content:encoded>")){
 		    expectContents = true;
 		} else if(expectContents && (!line.contains("<![CDATA["))){
 		    if(line.contains("]]>")){
-			//this line is CDATA close
-			expectContents = false;
+			//contents ended, output the collected event to file
+			String fmTop = "---\n";
+			String fmTitle = "title: " + eventTitle.replace(":", "") + "\n";
+			String fmDate = "date: " + pubDate + "\n";
+			String fmLayout = "layout: post\n";
+			String fmBottom = "---\n\n";
+			
+			try{
+			    File outputFile = new File("md-events", fileName);
+			    if(outputFile.exists()) {
+				//if file exists, recreate it
+				outputFile.delete();
+				outputFile.createNewFile();
+			    } else{
+				//doesn't exist, create it
+				outputFile.createNewFile();
+			    }
+			    FileOutputStream fos = new FileOutputStream(outputFile);
+			    try{
+				fos.write(fmTop.getBytes());
+				fos.write(fmTitle.getBytes());
+				fos.write(fmDate.getBytes());
+				fos.write(fmLayout.getBytes());
+				fos.write(fmBottom.getBytes());
+				fos.write(contents.getBytes());
+				fos.flush();
+				fos.close();
+
+				//reset for next event collection
+				eventTitle = "";
+				fileName = "";
+				pubDate = "";
+				contents = "";
+				expectContents = false;
+			    } catch(IOException e){
+				System.out.println("@@@IOException 1");
+			    } finally{
+				try{
+				    if(fos != null){
+					fos.close();
+				    }
+				} catch(IOException e){
+				    System.out.println("@@@IOException 2");
+				}
+			    }
+			} catch(IOException e){
+			    System.out.println("@@@IOException 3");
+			}
 		    } else{
 			//this line is content
-			contents = line;
+			contents = contents.concat(line);
 		    }
 		} else if(line.contains("<item>")){
 		    expectTitle = true;
 		}
-
-		//output to md file
-		try{
-		    String filePath = "md-events/" + fileName;
-		    File mayNotExist = new File(filePath);
-		    if (!mayNotExist.exists()) {
-			mayNotExist.createNewFile();
-		    }
-		    FileWriter fw = new FileWriter(filePath);
-		    BufferedWriter bw = new BufferedWriter(fw);
-		    try{
-			bw.write("---\n");
-			bw.write("title: " + eventTitle + "\n");
-			bw.write("date: " + pubDate + "\n");
-			bw.write("layout: post\n\n");
-			bw.write("---\n");
-			bw.write(contents);
-		    } catch(IOException e){
-			System.out.println("@@@IOException 1");
-		    } finally{
-			try{
-			    fw.close();
-			    bw.close();
-			} catch(IOException e){
-			    System.out.println("@@@IOException 2");
-			}
-		    }
-		} catch(IOException e){
-		    System.out.println("@@@IOException 3");
-		}
 	    }
 	    scan.close();
-	}
-	catch(FileNotFoundException e){
+	} catch(FileNotFoundException e){
 	    System.out.println("@@@FileNotFoundException");
 	}
     }
 
-    public static String toFileName(String title){
+    public static String toFileName(String title, String date){
 	//format event title to file name conventions
 	title = title.toLowerCase();
 	title = title.replace(' ', '-');
 	title = title.replace("@", "at");
 	title = title.replace(":", "");
 	title = title.replace("&#039;", "");
+	title = title.replace("!", "");
+
+	//prefixed with jekyll post date
+	title = toDatePrefix(date) + "-" + title;
 
 	//don't overwrite existing md event if it has same name
-	File mayExist = new File("md-events/" + title + ".md");
+	File mayExist = new File("md-events", title + ".md");
 	
 	if(mayExist.exists()){
 	    title = title.concat("-2.md");
@@ -111,8 +135,67 @@ public class Extractor{
 	return title;
     }
 
-    public static String toFMdate(String date){
-	//format wp date to front matter date format
-	return date;
+    public static String toDatePrefix(String date){
+	//format wp date to filename date format
+	//wp date is: Mon, 02 Oct 2017 19:17:10 +0000
+	//change to post name y/m/d date: 2017-10-02
+	String year = date.substring(12, 16);
+	String month = monthAbbrToNum(date.substring(8, 11));
+	String day = date.substring(5, 7);
+	return year + "-" + month + "-" + day;
+    }
+
+    public static String toFrontMatterDate(String date){
+	//format wp date to markdown front matter date format
+	//wp date is: Mon, 02 Oct 2017 19:17:10 +0000
+	//change to jekyll y/m/d date: 2017-10-02 19:17:10 +0000
+	String year = date.substring(12, 16);
+	String month = monthAbbrToNum(date.substring(8, 11));
+	String day = date.substring(5, 7);
+	String time = date.substring(17);
+	return year + "-" + month + "-" + day + " " + time;
+    }
+
+    public static String monthAbbrToNum(String month){
+	//month abbreviation to month number
+	switch(month.toLowerCase()){
+	    case "jan": {
+		return "01";
+	    }
+	    case "feb": {
+		return "02";
+	    }
+	    case "mar": {
+		return "03";
+	    }
+	    case "apr": {
+		return "04";
+	    }
+	    case "may": {
+		return "05";
+	    }
+	    case "jun": {
+		return "06";
+	    }
+	    case "jul": {
+		return "07";
+	    }
+	    case "aug": {
+		return "08";
+	    }
+	    case "sep": {
+		return"09";
+	    }
+	    case "oct": {
+		return "10";
+	    }
+	    case "nov": {
+		return "11";
+	    }
+	    case "dec": {
+		return "12";
+	    }
+	}
+	return "error";
     }
 }
